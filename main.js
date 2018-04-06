45c3578cf6a79bc751f1da4d0275fdfa95cc69fa
colorlessenergy <brianmunozdev@gmail.com>
2018-04-06 12:45:00 -0700

    add event for nav to change color when scrolled down

--- main.js
+++ main.js
@@ -7,9 +7,30 @@ hamburgerMenu.addEventListener("click", function () {
 });
 
 function animateHamburgerMenu(svg) {
-
-  console.log("called");
   svg.forEach(function(current, index) {
     current.classList.toggle('hamburger-menu__rect' + (index + 1));
   })
-}
+};
+
+var links = document.querySelectorAll("ul li a");
+var title = document.querySelector(".title a")
+var svgChicken = document.querySelector(".center__svg")
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 500) {
+    links.forEach(function (link) {
+      link.classList = "nav-color"
+    })
+    svgChicken.classList = "nav-color center__svg";
+    title.classList = "nav-color";
+
+    hamburgerMenu.classList = "nav-color";
+  } else if (window.scrollY <= 500) {
+    links.forEach(function (link) {
+      link.classList = "";
+    })
+    svgChicken.classList = "center__svg";
+    title.classList = "";
+    hamburgerMenu.classList = "";
+  }
+});
